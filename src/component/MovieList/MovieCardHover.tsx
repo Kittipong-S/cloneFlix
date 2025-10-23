3bd52c9600e0a137c199185cbb7c5f69750d6066
Kittipong Sangariyanan <49946004+Kittipong-S@users.noreply.github.com>
2025-10-23 16:18:17 +0700

    hover and popup card movie
    movieCardHover.tsx

--- src/component/MovieList/MovieCardHover.tsx
+++ src/component/MovieList/MovieCardHover.tsx
@@ -1,9 +1,9 @@
 const IMAGE_BASE_URL ='https://image.tmdb.org/t/p/original'
 function MovieCardHover({movie}) {
   return (
-    <div className="absolute">
-      <img src={IMAGE_BASE_URL+movie.backdrop_path}  className="w-[110px] md:w-[280px] rounded-sm hover:border-[3px] border-gray-400
-       duration-150 cursor-pointer "/>
+  <div className="absolute transition  duration-300 ease-in-out hover:-translate-y-1 hover:scale-100" > 
+      <img src={IMAGE_BASE_URL+movie.backdrop_path}  className="  w-[250] md:w-[500px] 
+       border-gray-400 cursor-pointer "/>
     </div>
   )
 }
