9e82b1253f9f08904fcc5c316e4c68cf24fce452
Kittipong Sangariyanan <49946004+Kittipong-S@users.noreply.github.com>
2025-10-29 15:23:16 +0700

    Hotmovietoday.tsx , MovieCardHover.tsx()
    ปรับสีรูปในflie Hotmovietoday.tsx ให้fade สีดำด้สนล่างใต้ภาพ
    ปรับเปลี่ยนให้หน้า MovieCardHover.tsx มาอยู่หน้าเดียวกับ Hotmoivetoday.tsx เพื่อการจัดการง่ายๆ

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div >
     <Navbar/>
     <Hotmovietoday/>
     <GenerMoiveList/>
@@ -34,7 +34,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p> */}
-    </>
+    </div>
   )
 }
 
--- src/component/HotMovie/Hotmovietoday.tsx
+++ src/component/HotMovie/Hotmovietoday.tsx
@@ -1,29 +1,37 @@
-import { useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import GobalApi from '../../../src/servic/service'
-const IMAGE_BASE_URL ='https://image.tmdb.org/t/p/original'
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/original'
+import GenerMoiveList from '../gener/GenerMoiveList';
 
 function Hotmovietoday() {
-const [movieList, setMovieList] = useState([])
-    useEffect(() => {
-        gerMovieVideo();
-    }, [])
+  const [movieList, setMovieList] = useState([])
+  useEffect(() => {
+    gerMovieVideo();
+  }, [])
 
-    const gerMovieVideo = () => [
-        GobalApi.getMovieVideodb.then(resp => {
-            // console.log(resp.data.results)
-            setMovieList(resp.data.results)
+  const gerMovieVideo = () => [
+    GobalApi.getMovieVideodb.then(resp => {
+      // console.log(resp.data.results)
+      setMovieList(resp.data.results)
 
-        })
+    })
 
-    ]
-    console.log(movieList[0]);
-    
+  ]
+  console.log(movieList[0]);
+
   return (
     <div className=''>
-    
- <img src={IMAGE_BASE_URL+"https://image.tmdb.org/t/p/original/1leYKN0DPNffpldGnCWnbXaiWoD.jpg"} className=''/>
+      <div className='h-180 bg-gradient-to-b to-gray-600'>
 
-  
+        <img src={IMAGE_BASE_URL + "https://image.tmdb.org/t/p/original/1leYKN0DPNffpldGnCWnbXaiWoD.jpg"} className='w-full object-cover ' />
+
+
+      </div>
+      <div className='relative'>
+        <div>
+<GenerMoiveList/>
+</div>
+      </div>
     </div>
   )
 }
