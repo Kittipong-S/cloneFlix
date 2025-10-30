df2a9f6e699c8b76d4c534b337818f3dcbce12f6
Kittipong Sangariyanan <49946004+Kittipong-S@users.noreply.github.com>
2025-10-30 15:57:29 +0700

    hoytMovieToday.tsx, Navbar.tsx
    เพิ่ม เพิ่มปุ้ม เล่น และ ข้อมูลเพิ่มเติมในหน้า HotmovieToday , ปรับแก้ Navbar ที่หายไปเมื่อเลื่อนหน้าจอลง

--- src/component/HotMovie/Hotmovietoday.tsx
+++ src/component/HotMovie/Hotmovietoday.tsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from 'react';
 import GobalApi from '../../../src/servic/service'
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/original'
 import GenerMoiveList from '../gener/GenerMoiveList';
+import { FaPlay } from "react-icons/fa";
+import { IoIosInformationCircleOutline } from "react-icons/io";
+
 
 function Hotmovietoday() {
   const [movieList, setMovieList] = useState([])
@@ -21,16 +24,33 @@ function Hotmovietoday() {
 
   return (
     <div className=''>
-      <div className='h-180 bg-gradient-to-b to-gray-600'>
-
+      <div className='h-180 bg-gradient-to-b to-gray-600 relative '>
         <img src={IMAGE_BASE_URL + "https://image.tmdb.org/t/p/original/1leYKN0DPNffpldGnCWnbXaiWoD.jpg"} className='w-full object-cover ' />
-
+        <div className='absolute border border-amber-700 bottom-4 ml-18 mb-30 '>
+          <div className=''>
+asdasdasdasdsa
+          </div>
+          <div className='flex gap-5 '>
+          <div className='flex justify-between gap-4  rounded-md  px-7  items-center bg-gray-100 hover:bg-gray-300 text-black cursor-pointer'>
+            <div className='text-[28px]'>
+            <FaPlay />
+            </div>
+            <p className='text-[25px]'>เล่น</p>
+          </div>
+          <div className='flex justify-between gap-5  rounded-md text-white p-3 items-center bg-gray-600/75 hover:bg-gray-600/50 cursor-pointer'>
+            <div className='text-[35px]'>
+            <IoIosInformationCircleOutline />
+            </div>
+            <p className='text-[25px]'>ข้อมูลเพิ่มเติม</p>
+          </div>
+          </div>
+        </div>
 
       </div>
       <div className='relative'>
         <div>
-<GenerMoiveList/>
-</div>
+          <GenerMoiveList />
+        </div>
       </div>
     </div>
   )
--- src/component/header/Navbar.tsx
+++ src/component/header/Navbar.tsx
@@ -36,7 +36,7 @@ function Navbar() {
     }
     ]
     return (
-        <div className="sticky top-0 flex gap-5  justify-between p-4 navbarColor" >
+        <div className="sticky top-0 flex gap-5  justify-between p-4 navbarColor z-10" >
             <div className="flex gap-8 items-center pl-10 ">
                 <img src={CloneFlix} className="w-[70px] md:w-[100px] object-cover cursor-pointer" />
                 <div className="flex gap-8 items-center">
